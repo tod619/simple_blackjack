a9b485668bd7c5ef31dacf40524f979823d2d469
tod619 <trevorodwyer619@hotmail.com>
2023-05-30 21:11:54 +0100

    Create simple deal card function in run.py

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 # Simple Blackjack
 # A simple game of blackjack to explore the game logic
 # 30/05/2023
+import random
+
+user_cards = []
+computer_cards = []
+
+
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
