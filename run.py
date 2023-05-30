08f3e0aa3f18559d153e50f37508baa7644bec2d
tod619 <trevorodwyer619@hotmail.com>
2023-05-30 21:23:18 +0100

    deal the player + computer 2 cards each

--- run.py
+++ run.py
@@ -8,6 +8,22 @@ computer_cards = []
 
 
 def deal_card():
-    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    card = random.choice(cards)
-    return card
+    """ Return a random card from the deck """
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]  # simple card deck
+    card = random.choice(cards)  # shuffle deck
+    return card  # return random card
+
+
+# my_card = deal_card()
+# print(my_card)
+
+# deal the computer + user 2 cards each
+for round in range(2):
+    # new_card = deal_card()
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+print(user_cards)
+print(computer_cards)
+
+input("\n\nPress enter to exit ")
